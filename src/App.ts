69f9582247087d0d043163ce41258c179aed2a7e
Sporysz, Edyta <edyta.sporysz@sap.com>
2018-08-13 08:46:27 +0200

    create branch step-0

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,6 @@
-import { Cached } from 'cache/cached';
-import { CachedObserver } from 'cache/cached-observer';
-import { Cold } from './hot_and_cold/cold';
+
 export class App {
   run() {
-    console.log('Cache');
-    Cached.init();
-    const observer1 = new CachedObserver('A');
-    let observer2 = new CachedObserver('B');
-    observer2 = new CachedObserver('C');
-    console.log('Hot&Cold');
-    const cold = new Cold();
+    console.log('Learn Rxjs');
   }
 }
--- src/cache/cached-observer.ts
+++
@@ -1,9 +0,0 @@
-import { Cached } from './cached';
-
-export class CachedObserver {
-  constructor(private name: string) {
-    Cached.cached$.subscribe((value) => {
-      console.log(`Observer ${this.name} next value: ${value}`);
-    });
-  }
-}
