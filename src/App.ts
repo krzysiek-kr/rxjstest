6f6c746b6e00f1f85028e0653e0258189da39932
Sporysz, Edyta <edyta.sporysz@sap.com>
2018-09-14 09:41:35 +0200

    prepare exercises for basic operators

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,75 @@
 import { MyObservable } from 'my-observable';
+import { map, filter, flatMap } from 'rxjs/operators';
+import { getTranslations } from './data';
 
 export class App {
   run() {
-    MyObservable.init();
-    MyObservable.map();
-    MyObservable.filter();
-    MyObservable.subscribe();
-    MyObservable.init();
-    MyObservable.joinAllOperators();
-    MyObservable.subscribe();
-    MyObservable.init();
-    MyObservable.flatMapOperators();
-    MyObservable.subscribe();
+
+
+    // ex 1: pass each 'x' source value through a transformation function to get 'x + 5' output value
+
+    const observableClass1 = new MyObservable();
+    observableClass1.observable$.pipe(
+      /* ... */
+    ).subscribe({
+      next: (value) => {
+        console.log('value: ' + value);
+      },
+      error: (error) => {
+        console.error('error: ' + error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+
+    // ex 2: pass each 'x' source value through a transformation function to get only 'x % 2 === 0' values
+
+    const observableClass2 = new MyObservable();
+    observableClass2.observable$.pipe(
+      /* ... */
+    ).subscribe({
+      next: (value) => {
+        console.log('value: ' + value);
+      },
+      error: (error) => {
+        console.error('error: ' + error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+
+    // ex 3: join operators you use in ex 1 and ex 2
+    const observableClass3 = new MyObservable();
+    observableClass3.observable$.pipe(
+      /* ... */
+    ).subscribe({
+      next: (value) => {
+        console.log('value: ' + value);
+      },
+      error: (error) => {
+        console.error('error: ' + error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+
+    // ex 4: translate numbers into string, use getTranslation method
+    const observableClass4 = new MyObservable();
+    observableClass4.observable$.pipe(
+      /* ... */
+    ).subscribe({
+      next: (value) => {
+        console.log('value: ' + value);
+      },
+      error: (error) => {
+        console.error('error: ' + error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
   }
 }
--- src/my-observable.ts
+++ src/my-observable.ts
@@ -4,12 +4,9 @@ import { map, filter, flatMap } from 'rxjs/operators';
 
 export class MyObservable {
 
-  static observable$: Observable<any>;
+  observable$: Observable<any>;
 
   constructor() {
-  }
-
-  static init() {
     this.observable$ = new Observable<number>((observer) => {
       for (let i = 0; i < DATA_1.length; i++) {
         observer.next(DATA_1[i]);
@@ -18,43 +15,4 @@ export class MyObservable {
     });
   }
 
-  static map() {
-    this.observable$ = this.observable$.pipe(map<number, number>((value) => {
-      return value + 5;
-    }));
-  }
-
-  static filter() {
-    this.observable$ = this.observable$.pipe(filter((value) => {
-      return value % 2 === 0;
-    }));
-  }
-
-  static joinAllOperators() {
-    const mapOperation = map<number, number>((value) => {
-      return value + 5;
-    });
-    const filterOperation = filter((value: number) => {
-      return value % 2 === 0;
-    });
-
-    this.observable$ = this.observable$.pipe(mapOperation, filterOperation);
-  }
-
-  static flatMapOperators() {
-    const flatMapOperation = flatMap<number, string>((value) => {
-      return getTranslations(value);
-    });
-    this.observable$ = this.observable$.pipe(flatMapOperation);
-  }
-
-  static subscribe() {
-    this.observable$.subscribe(
-      {
-        next: x => console.log('got value ' + x),
-        error: err => console.error('something wrong occurred: ' + err),
-        complete: () => console.log('done'),
-      }
-    );
-  }
 }
