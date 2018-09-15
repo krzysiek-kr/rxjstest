450f0ef079e20f709365591ac6fb7311705373c1
Sporysz, Edyta <edyta.sporysz@sap.com>
2018-09-15 22:30:57 +0200

    prepare exercises for error catching

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,21 @@
 import { MyObservable } from 'my-observable';
+import { map, catchError, filter } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 export class App {
   run() {
-    MyObservable.init1();
-    MyObservable.catchErrors1();
-    MyObservable.init2();
-    MyObservable.catchErrors2();
+
+    // Ex: map each emitted value through transformation 'x + 2', when error occurs return 100, finally filter
+    // values greater than 5
+    const observableClass = new MyObservable();
+    observableClass.observable$.pipe(
+      /* ... */
+      .subscribe(
+        {
+          next: x => console.log('got value ' + x),
+          error: err => console.error('something wrong occurred: ' + err),
+          complete: () => console.log('done'),
+        }
+      );
   }
 }
--- src/my-observable.ts
+++ src/my-observable.ts
@@ -1,45 +1,11 @@
 import { Observable, of } from 'rxjs';
-import { map, catchError} from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 export class MyObservable {
 
-  static observable$: Observable<number>;
+  observable$: Observable<number>;
 
   constructor() {
-  }
-
-  static init1() {
-    this.observable$ = new Observable<number>((observer) => {
-      observer.next(1);
-      observer.next(2);
-      observer.next(3);
-      observer.next(4);
-      observer.next(5);
-    });
-  }
-  static catchErrors1() {
-    const mapOperation = map((value: number) => {
-      if (value % 2) {
-        throw new Error();
-      } else {
-        return value;
-      }
-    });
-    const catchOperation = catchError((error) => {
-      console.log(error);
-      return of(100);
-    });
-
-    this.observable$.pipe(mapOperation, catchOperation).subscribe(
-      {
-        next: x => console.log('got value ' + x),
-        error: err => console.error('something wrong occurred: ' + err),
-        complete: () => console.log('done'),
-      }
-    );
-  }
-
-  static init2() {
     this.observable$ = new Observable<number>((observer) => {
       observer.next(1);
       observer.next(2);
@@ -47,20 +13,6 @@ export class MyObservable {
       observer.next(4);
       observer.error(5);
     });
-  }
-  static catchErrors2() {
-    const catchOperation = catchError((error) => {
-      console.log(error);
-      return of(100);
-    });
-
-    this.observable$.pipe(catchOperation).subscribe(
-      {
-        next: x => console.log('got value ' + x),
-        error: err => console.error('something wrong occurred: ' + err),
-        complete: () => console.log('done'),
-      }
-    );
   }
 
 }
