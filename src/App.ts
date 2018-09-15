4a2499165f4d96b216d6102e3872ab40aa52e4ed
Sporysz, Edyta <edyta.sporysz@sap.com>
2018-09-15 23:01:28 +0200

    prepare exercises for unsubsription

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,29 @@
-import { Unsubscription } from './unsubsription/unsubscripton';
+import { InfiniteObservable1, InfiniteObservable2 } from './unsubsription/unsubscripton';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 export class App {
   run() {
-    const unsubscribe = new Unsubscription();
+
+    // Ex 1: subscribe to InfiniteObservable1 and unsubscribe after 3 seconds
+    const obs1 = new InfiniteObservable1();
+    const sub1 = obs1.observable$.subscribe({
+      next: v => console.log('1st subscriber: ' + v),
+      complete: () => console.log('complete')
+    });
+    /* ... */
+
+    // Ex 2: subscribe to InfiniteObservable2 and unsubscribe when toDestroy$ observable is completed
+
+    const obs2 = new InfiniteObservable2();
+    const sub2 = obs2.observable$.pipe(
+      /*...*/
+    ).subscribe({
+      next: v => console.log('1st subscriber: ' + v),
+      complete: () => console.log('complete')
+    }
+    );
+    setTimeout(() => {
+      obs2.toDestroy$.next();
+      obs2.toDestroy$.complete();
+    }, 3000);
   }
 }
--- src/unsubsription/unsubscripton.ts
+++ src/unsubsription/unsubscripton.ts
@@ -1,45 +1,20 @@
 import { interval } from 'rxjs/internal/observable/interval';
-import { Subscription, Subject } from 'rxjs';
+import { Subscription, Subject, Observable } from 'rxjs';
 import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 
-export class Unsubscription {
+export class InfiniteObservable1 {
 
-  subscription: Subscription;
-
-  // uncomment method to show differences between different ways of unsubscription
-  // from infinite source of observable
+  observable$;
   constructor() {
-  //   this.infinite1();
-    this.infinite2();
+    this.observable$ = interval(1000);
   }
+}
 
-  infinite1() {
-    let obs;
-    obs = interval(1000);
+export class InfiniteObservable2 {
 
-    this.subscription = obs.subscribe({
-      next: v => console.log('1st subscriber: ' + v),
-      complete: () => console.log('complete')
-    }
-    );
-
-    setTimeout(() => this.subscription.unsubscribe(), 3000);
-  }
-
-  infinite2() {
-    let obs;
-    const destroy$ = new Subject();
-    obs = interval(1000);
-
-    obs.pipe(takeUntil(destroy$)).subscribe({
-      next: v => console.log('1st subscriber: ' + v),
-      complete: () => console.log('complete')
-    }
-    );
-
-    setTimeout(() => {
-      destroy$.next();
-      destroy$.complete();
-    }, 3000);
+  observable$;
+  toDestroy$ = new Subject();
+  constructor() {
+    this.observable$ = interval(1000);
   }
 }
