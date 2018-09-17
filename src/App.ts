8800f5d69df297e600ed5c89d719d42301d0d1ca
Sporysz, Edyta <edyta.sporysz@sap.com>
2018-09-17 14:53:21 +0200

    prepare exercises for combined operators

--- src/App.ts
+++ src/App.ts
@@ -7,20 +7,8 @@ export class App {
     // ex 1: combine each value emitted by two observables to get x + y
     const observableClass1 = new MyObservable1();
     const observableClass2 = new MyObservable2();
-    // ... //
-    .subscribe(
-      {
-        next: x => console.log('got value ' + x),
-        error: err => console.error('something wrong occurred: ' + err),
-        complete: () => console.log('done'),
-      }
-    );
-
-    // ex 2: use forkJoin to combine values emitted by two observables to get x + y
-    const observableClass3 = new MyObservable1();
-    const observableClass4 = new MyObservable2();
-    // ... //
-    .subscribe(
+    /* ... */
+      .subscribe(
       {
         next: x => console.log('got value ' + x),
         error: err => console.error('something wrong occurred: ' + err),
